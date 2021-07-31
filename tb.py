7de8d34fb672d19944b8cc9d39a2ee1e792fd563
Alex <alex@tinkerlog.com>
2021-07-31 15:28:10 +0200

    split off tbp

--- tb.py
+++ tb.py
@@ -30,7 +30,6 @@ ABS = 'ABS'
 LIST = 'LIST'
 RUN = 'RUN'
 CLEAR = 'CLEAR'
-FREEZE = 'FREEZE'
 STRING = 'STRING'
 NUMBER = 'NUMBER'
 FLOAT = 'FLOAT'
@@ -80,15 +79,13 @@ LANGUAGE_KEYWORDS = {
     END:    Token(END),
     LIST:   Token(LIST),
     RUN:    Token(RUN),
-    CLEAR:  Token(CLEAR),
-    FREEZE: Token(FREEZE)
+    CLEAR:  Token(CLEAR)
 }
 
 IMMEDIATE_KEYWORDS = {
     LIST,
     RUN,
-    CLEAR,
-    FREEZE
+    CLEAR
 }
 
 
@@ -731,7 +728,6 @@ class TinyBasic(object):
                 self.output.write(f"{line_number} {statement}\n")
         elif command == RUN: self.run()
         elif command == CLEAR: self.memory.clear()
-        elif command == FREEZE: self.export_state()
         self.output.write("OK\n")
 
     def repl(self):
@@ -766,20 +762,13 @@ class TinyBasic(object):
             file.write(export_json)
 
 
-def run(source_filename, input_filename=None, in_state_filename=None, out_state_filename = None):
-    print(f"run source: {source_filename}, input: {input_filename}, in state: {in_state_filename}, out state: {out_state_filename}")
-    input_file = open(input_filename, 'r') if input_filename else sys.stdin
-    if in_state_filename:
-        pass
-    elif source_filename:
-        with open(source_filename, 'r') as file:
-            raw_lines = file.readlines()
-        tiny_basic = TinyBasic(input_file, sys.stdout, {}, [], 0, raw_lines)
+def run(source_filename):
+    with open(source_filename, 'r') as file:
+        raw_lines = file.readlines()
+    tiny_basic = TinyBasic(sys.stdin, sys.stdout, {}, [], 0, raw_lines)
     try:
         tiny_basic.parse_all()
         tiny_basic.run()
-    except EOFError: # not enough input, so we stop here for now
-        tiny_basic.export_state(out_state_filename)
     except Exception as e:
         print("ERROR: {}".format(e), file=sys.stderr)
 
@@ -789,30 +778,10 @@ def repl():
 
 def main(argv):
     print("Tiny Basic v0.1")
-    # "tb"                             --> repl mode, interactive
-    # "tb sample.bas"                  --> run sample.bas
-    # "tb -input input.txt sample.bas" --> run sample.bas and use input.txt as input
-    # "tb -state state.json"           --> run from saved state
-    source_file = None
-    input_file = None
-    in_state_file = None
-    out_state_file = None
-    while argv:
-        arg = argv.pop(0)
-        if arg == "-input":
-            input_file = argv.pop(0)
-        elif arg == "-in_state":
-            in_state_file = argv.pop(0)
-        elif arg == "-out_state":
-            out_state_file = argv.pop(0)
-        else:
-            if source_file == None: source_file = arg
-            else: raise Exception(f"don't know how to handle {arg}")
-
-    if not source_file and not in_state_file:
-        repl()
+    if len(argv) == 1:
+        run(argv[0])
     else:
-        run(source_file, input_file, in_state_file, out_state_file)
+        repl()
 
 if __name__ == '__main__':
     import sys
